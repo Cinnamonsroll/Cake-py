a7034a607a251374a45d916b4c6bb9aa9b17372d
Cinnamonsroll <cinnamonsrowoll@gmail.com>
2021-07-09 14:09:02 -0400

    TAG COMMAND DONE HEHE

--- commands/utility/tags.py
+++ commands/utility/tags.py
@@ -1,31 +1,119 @@
-from discord.ext import commands
 import discord
+from discord.ext import commands
 class Tags(commands.Cog):
     def __init__(self, client):
         self.client = client
-    @commands.group(name="tags", description = "View, list, create, update, or delete a server tag", aliases=["t", "tag"])
+
+    @commands.group(name="tags", description="View, list, create, update, or delete a server tag", aliases=["t", "tag"])
     @commands.guild_only()
     async def tags(self, ctx):
         if ctx.invoked_subcommand is None:
-            await ctx.send("Please provide a valid subcommand either `add`, `delete`, `rename`, `edit`, `alias`, or `use`")
+            await ctx.send("Please provide a valid subcommand either `add`, `delete`, `edit`, `alias`, or `use`")
+
     @tags.command(name="use", description="Use a tag", aliases=["u"])
-    async def use(self, ctx, tag = None):
-        guildData = self.client._guilds.find_one({"guild": str(ctx.message.guild.id)})
-        if tag == None or not len(list(filter(lambda x: x["name"] == tag, guildData["tags"]))) > 0:
+    async def use(self, ctx, tag=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if tag == None or not len(list(filter(lambda x: x["name"] == tag or tag in x["aliases"], guildData["tags"]))) > 0:
             return await ctx.send("Tag not found")
-        else: 
-            foundTag = list(filter(lambda x: x["name"] == tag, guildData["tags"]))[0]
+        else:
+            foundTag = list(
+                filter(lambda x: x["name"] == tag or tag in x["aliases"], guildData["tags"]))[0]
             return await ctx.send(foundTag["content"])
-    @tags.command(name="add", description="Add a tag", aliases=["create"])
-    @commands.has_permissions(manage_messages = True)
-    async def add(self, ctx, name = None, content = None):
-        guildData = self.client._guilds.find_one({"guild": str(ctx.message.guild.id)})
+    @tags.command(name="view", description="View all tags", aliases=["list"])
+    async def view(self, ctx):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if len(guildData["tags"]) <= 0:
+            return await ctx.send("This server has no tags")
+        tags = [guildData["tags"][i:i+10]
+                for i in range(0, len(guildData["tags"]), 10)]
+        embeds = []
+        for tag in tags:
+            embed = discord.Embed(color=self.client.color, title="Tags",
+                                  description=f"{', '.join(['`' + x['name'] + '`' for x in tag])}")
+            embeds.append(embed)
+        await self.client.pagination(self.client, ctx, ctx.message, embeds)
+
+    @tags.command(name="delete", description="Deletes a tag", aliases=["remove"])
+    @commands.has_permissions(manage_messages=True)
+    async def add(self, ctx, name=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if not name:
+            return await ctx.send("Please include a valid name")
+        if len(list(filter(lambda x: x["name"] == name, guildData["tags"]))) <= 0:
+            return await ctx.send("Tag doesn't exist")
+        foundTag = list(
+            filter(lambda x: x["name"] == name, guildData["tags"]))[0]
+        if not foundTag["owner"] == ctx.message.author.id:
+            return await ctx.send("You do not own this tag")
+        guildData["tags"] = list(
+            filter(lambda x: not x["name"] == name, guildData["tags"]))
+        self.client._guilds.find_one_and_update(
+            {"guild": str(ctx.guild.id)}, {"$set": guildData})
+        return await ctx.send("Tag deleted")
+
+    @tags.command(name="edit", description="Edits a tag", aliases=["update"])
+    @commands.has_permissions(manage_messages=True)
+    async def edit(self, ctx, name=None, *, content=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
         if not name or not content:
             return await ctx.send("Please include a valid name and content")
-        if len(list(filter(lambda x: x["name"] == name, guildData["tags"]))) > 0 or name in ["create", "add", "use", "u"]:
+        if len(list(filter(lambda x: x["name"] == name, guildData["tags"]))) <= 0:
+            return await ctx.send("Tag doesn't exist")
+        foundTag = list(
+            filter(lambda x: x["name"] == name, guildData["tags"]))[0]
+        if not foundTag["owner"] == ctx.message.author.id:
+            return await ctx.send("You do not own this tag")
+        guildData["tags"][next((index for (index, d) in enumerate(
+            guildData["tags"]) if d["name"] == name), None)]["content"] = content
+        self.client._guilds.find_one_and_update(
+            {"guild": str(ctx.guild.id)}, {"$set": guildData})
+        return await ctx.send("Tag edited")
+    @tags.command(name="add", description="Adds a tag", aliases=["create"])
+    @commands.has_permissions(manage_messages=True)
+    async def add(self, ctx, name=None, *, content=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if not name or not content:
+            return await ctx.send("Please include a valid name and content")
+        if len(list(filter(lambda x: x["name"] == name or name in x["aliases"], guildData["tags"]))) > 0 or name in ["create", "add", "use", "u", "view", "list", "delete", "remove", "edit", "update", "alias", "meta", "info"]:
             return await ctx.send("Tag already exists or restricted name")
-        guildData["tags"].append({"name": name, "content": content, "owner": ctx.message.author.id, "aliases": []})
-        self.client._guilds.find_one_and_update({"guild": str(ctx.message.guild.id)}, {"$set": guildData})
+        guildData["tags"].append(
+            {"name": name, "content": content, "owner": ctx.message.author.id, "aliases": []})
+        self.client._guilds.find_one_and_update(
+            {"guild": str(ctx.guild.id)}, {"$set": guildData})
         return await ctx.send("Tag created")
+    @tags.command(name="alias", description="Add an alias to a tag")
+    @commands.has_permissions(manage_messages=True)
+    async def alias(self, ctx, name=None, alias=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if not name or not alias:
+            return await ctx.send("Please include a valid name and alias")
+        if len(list(filter(lambda x: x["name"] == name, guildData["tags"]))) <= 0:
+            return await ctx.send("Tag doesn't exist")
+        foundTag = list(
+            filter(lambda x: x["name"] == name, guildData["tags"]))[0]
+        if not foundTag["owner"] == ctx.message.author.id:
+            return await ctx.send("You do not own this tag")
+        guildData["tags"][next((index for (index, d) in enumerate(
+            guildData["tags"]) if d["name"] == name), None)]["aliases"].append(alias)
+        self.client._guilds.find_one_and_update(
+            {"guild": str(ctx.guild.id)}, {"$set": guildData})
+        return await ctx.send("Alias added")
+    @tags.command(name="meta", description="Get info on a tag", aliases=["tag"])
+    async def meta(self, ctx, name=None):
+        guildData = self.client._guilds.find_one(
+            {"guild": str(ctx.guild.id)})
+        if not name:
+            return await ctx.send("Please include a valid name and alias")
+        if len(list(filter(lambda x: x["name"] == name or name in x["aliases"], guildData["tags"]))) <= 0:
+            return await ctx.send("Tag doesn't exist")
+        foundTag = list(
+            filter(lambda x: x["name"] == name or name in x["aliases"], guildData["tags"]))[0]
+        return await ctx.send(f"Name: {foundTag['name']}\nContent: {foundTag['content']}\nAliases: {'None' if len(foundTag['aliases']) <= 0 else ', '.join(foundTag['aliases'])}\nOwner: {self.client.get_user(foundTag['owner'])}")
 def setup(client):
     client.add_cog(Tags(client))
--- functions/pagination.py
+++ functions/pagination.py
@@ -5,8 +5,9 @@ from discord_components import (
     Select,
     SelectOption,
 )
-def createButtons(disabledComponents, client): 
-    return [
+def createButtons(disabledComponents, client, embedsLen): 
+    if embedsLen > 1:
+        return  [
         [
             Button(style=ButtonStyle.red, disabled=disabledComponents, custom_id="Delete", label="\u200b", emoji=client.get_emoji(848216792845516861)),
             Button(style=ButtonStyle.blue, disabled=disabledComponents, custom_id="Backward", label="\u200b", emoji=client.get_emoji(848237448269135924)),
@@ -14,11 +15,16 @@ def createButtons(disabledComponents, client):
             Button(style=ButtonStyle.blue, disabled=disabledComponents, custom_id="Forward", label="\u200b", emoji=client.get_emoji(848237230363246612)),
         ]
     ]
+    else: return  [
+        [
+            Button(style=ButtonStyle.red, disabled=disabledComponents, custom_id="Delete", label="\u200b", emoji=client.get_emoji(848216792845516861)),
+        ]
+    ]
 async def pagination(client, ctx, message, embeds):
     page = 0
     going = True
     disabledComponents = False
-    mainMessage = await ctx.send(embed=embeds[page], components=createButtons(False, client))
+    mainMessage = await ctx.send(embed=embeds[page], components=createButtons(False, client, len(embeds)))
     while going:
         interaction = await client.wait_for("button_click", check = lambda x: x.user.id == message.author.id)
         if interaction.custom_id == "Delete":
@@ -33,4 +39,4 @@ async def pagination(client, ctx, message, embeds):
         elif interaction.custom_id == "Forward":
             if page == len(embeds) - 1: page = 0
             else: page += 1
-        await interaction.respond(type=7, embed=embeds[page], components=createButtons(disabledComponents, client))
+        await interaction.respond(type=7, embed=embeds[page], components=createButtons(disabledComponents, client, len(embeds)))
